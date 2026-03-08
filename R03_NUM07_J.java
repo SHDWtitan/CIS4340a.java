62490352c69fb338f1bbbb25cf75e61aff9e3735
SHDWtitan <Hunterlayf@gmail.com>
2026-03-08 13:53:00 -0400

    Create R03_NUM07_J.java

---
+++ R03_NUM07_J.java
@@ -0,0 +1,9 @@
+public class NaNComparison {
+  public static void main(String[] args) {
+    double x = 0.0;	  
+    double result = Math.cos(1/x); // Returns NaN when input is infinity
+    if (Double.isNaN(result)) { 
+      System.out.println("result is NaN");
+    }
+  }
+}
